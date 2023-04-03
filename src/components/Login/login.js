ba0d92f24c1385d22a602674bc0307e044fe414e
jayStackk <jmagpantay@codestack.co>
2023-04-03 10:37:37 -0700

    Quick merge to test branch

---
+++ src/components/Login/login.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './login.css';
+import {  Form, NavLink} from 'react-bootstrap';
+import background from "../assets/jiujitsu.png";
+
+function Login() {
+    return (
+
+    // <Container className='form2'>
+
+
+    
+        <div id="container" 
+        className='blue-border'
+        style={{ 
+        backgroundImage: `url(${background})`,
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        minWidth: "1145px",
+        minHeight: "700px",
+        borderTop: "10px solid rgba(220, 219, 252, 0.99)",
+        borderLeft: "10px solid rgba(220, 219, 252, 0.99)",
+        borderRight: "10px solid rgba(220, 219, 252, 0.99)",
+
+    }}>
+
+
+    
+        <Form style={{ position: 'relative',width: '503px', height:'579px', background: "rgba(255, 255, 255, 0.4)", display: 'flex', flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "54px auto 68px"}}>
+      <h1 style={{ fontWeight: "700", fontSize: "96px", lineHeight: "116px", margin: "0"}}> The Path </h1>
+      <p style={{ fontWeight: "700", fontSize: "32px", lineHeight: "39px"}}>A community driven Brazilian Jiu-Jitsu App</p>
+        
+        <Form.Control id='input' type="email" placeholder="Email" />
+
+        <Form.Control type="password" placeholder="Password" />
+        <div className='form-group form-buttons'>
+          <NavLink href="#">Forgot <span> password?</span></NavLink>
+          <NavLink href="#">Signup <span>here</span></NavLink>
+        </div>
+
+        <div className='form-group'>
+          <button type='submit' className='btn-pri'>Login</button>
+        </div>
+        </Form>
+      
+    </div>
+    // </Container>
+    );
+  }
+  export default Login;
